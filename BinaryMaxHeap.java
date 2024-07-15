f116d193f71968986005a09af497e250c62486d9
Khang Hoang Nguyen <96611672+KhangNguyen2005@users.noreply.github.com>
2024-07-15 19:57:58 +0700

    Add files via upload

---
+++ BinaryMaxHeap.java
@@ -0,0 +1,202 @@
+package assign10;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class BinaryMaxHeap<E> implements PriorityQueue<E> {
+	private E[] heap;
+	private int size;
+	private Comparator<? super E> comparator;
+
+	/**
+	 * Step 1) Add the item in the next available space. (satisfy structure
+	 * property),
+	 *
+	 * Step 2) Move it up the tree until the order is correct. (satisfy heap order
+	 * property) (Called percolate-up, bubble-up, sift-up, etc.)
+	 */
+	@Override
+	public void add(E item) {
+		if (size == heap.length)
+			resizeHeap();
+		heap[size] = item;
+		percolateUp(size);
+		size++;
+	}
+
+	@Override
+	public E peek() throws NoSuchElementException {
+		if (isEmpty())
+			throw new NoSuchElementException("Heap is empty");
+
+		return heap[0];
+	}
+
+	@Override
+	public E extractMax() throws NoSuchElementException {
+		if (isEmpty())
+			throw new NoSuchElementException();
+		E max = heap[0];
+		heap[0] = heap[size - 1];
+		size--;
+		percolateDown(0);
+		return max;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public void clear() {
+		this.heap = (E[]) new Object[16];
+		this.size = 0;
+	}
+
+	@Override
+	public Object[] toArray() {
+		Object[] array = new Object[size];
+		BinaryMaxHeap<E> temp = new BinaryMaxHeap<>(Arrays.asList(heap));
+		for (int i = 0; i < size; i++) {
+			array[i] = temp.extractMax();
+		}
+		return array;
+	}
+
+	/**
+	 * If this constructor is used to create an empty binary heap, it is assumed
+	 * that the elements are ordered using their natural ordering (i.e., E
+	 * implements Comparable<? super E>).
+	 */
+	public BinaryMaxHeap() {
+		heap = (E[]) new Object[16];
+		size = 0;
+		comparator = null;
+	}
+
+	/**
+	 * If this constructor is used to create an empty binary heap, it is assumed
+	 * that the elements are ordered using the provided Comparator object.
+	 *
+	 * @param cmp
+	 */
+	public BinaryMaxHeap(Comparator<? super E> cmp) {
+		heap = (E[]) new Object[16];
+		size = 0;
+		comparator = cmp;
+	}
+
+	/**
+	 * If this constructor is used, then the binary heap is constructed from the
+	 * given list. Also, it is assumed that the elements are ordered using their
+	 * natural ordering (i.e., E implements Comparable<? super E>).
+	 * <p>
+	 * RECALL: Using the buildHeap operation, we can construct a binary heap from
+	 * these N items in O(N) time, which is more efficient than adding them to the
+	 * binary heap one at a time. This constructor must use such an operation.
+	 *
+	 * @param list
+	 */
+	public BinaryMaxHeap(List<? extends E> list) {
+		heap = (E[]) new Object[list.size()];
+		size = list.size();
+		for (int i = 0; i < size; i++) {
+			heap[i] = list.get(i);
+		}
+		buildHeap();
+	}
+
+	/**
+	 * If this constructor is used, then the binary heap is constructed from the
+	 * given list (see RECALL note above). Also, it is assumed that the elements are
+	 * ordered using the provided Comparator object.
+	 *
+	 * @param list
+	 * @param cmp
+	 */
+	public BinaryMaxHeap(List<? extends E> list, Comparator<? super E> cmp) {
+		heap = (E[]) new Object[list.size()];
+		size = list.size();
+		comparator = cmp;
+		for (int i = 0; i < size; i++) {
+			heap[i] = list.get(i);
+		}
+		buildHeap();
+	}
+// private helper method buildHeap, percolateUp, percolateDown, innerCompare
+
+	private void buildHeap() {
+		// This is kinda sus, check it thoroughly or u cooked
+		for (int i = (heap.length - 1) / 2; i >= 0; i--) {
+			percolateDown(i);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void percolateUp(int index) {
+		// Get the parent index
+		int parentIndex = (index - 1) / 2;
+
+		// While the current element is greater than its parent
+		while (index > 0 && innerCompare(heap[index], heap[parentIndex]) > 0) {
+			// Swap the current element with its parent
+			E temp = heap[index];
+			heap[index] = heap[parentIndex];
+			heap[parentIndex] = temp;
+
+			// Update the current index to the parent index
+			index = parentIndex;
+
+			// Get the new parent index
+			parentIndex = (index - 1) / 2;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void percolateDown(int index) {
+		int leftChild = 2 * index + 1;
+		int rightChild = 2 * index + 2;
+		int largest = index;
+
+		if (leftChild < size && innerCompare(heap[leftChild], heap[largest]) > 0)
+			largest = leftChild;
+
+		if (rightChild < size && innerCompare(heap[rightChild], heap[largest]) > 0)
+			largest = rightChild;
+
+		if (largest != index) {
+			E temp = heap[index];
+			heap[index] = heap[largest];
+			heap[largest] = temp;
+			percolateDown(largest);
+		}
+	}
+
+	private void resizeHeap() {
+		int newCapacity = heap.length * 2;
+		E[] newHeap = (E[]) new Object[newCapacity];
+		System.arraycopy(heap, 0, newHeap, 0, heap.length);
+		heap = newHeap;
+	}
+
+	private int innerCompare(E obj1, E obj2) {
+		if (obj1 == null && obj2 == null)
+			return 0;
+		else if (obj1 == null)
+			return -1;
+		else if (obj2 == null)
+			return 1;
+		else if (comparator != null)
+			return comparator.compare(obj1, obj2);
+		else
+			return ((Comparable<? super E>) obj1).compareTo(obj2);
+	}
+}
---
+++ FindKLargest.java
@@ -0,0 +1,101 @@
+package assign10;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * This class contains generic static methods for finding the k largest items in a list.
+ * 
+ * @author Erin Parker and ??
+ * @version ??
+ */
+public class FindKLargest {
+	
+	/**
+	 * Determines the k largest items in the given list, using a binary max heap and the 
+	 * natural ordering of the items.
+	 * 
+	 * @param items - the given list
+	 * @param k - the number of largest items
+	 * @return a list of the k largest items, in descending order
+	 * @throws IllegalArgumentException if k is negative or larger than the size of the given list
+	 */
+	public static <E extends Comparable<? super E>> List<E> findKLargestHeap(List<E> items, int k) throws IllegalArgumentException {
+		if (k < 0 || k > items.size())
+			throw new IllegalArgumentException();
+		List<E> result = new ArrayList<>();
+		BinaryMaxHeap<E> maxHeap = new BinaryMaxHeap<>(items);
+		for (int i = 0; i < k; i++)
+			result.add(maxHeap.extractMax());
+		return result;
+	}
+
+	/**
+	 * Determines the k largest items in the given list, using a binary max heap.
+	 * 
+	 * @param items - the given list
+	 * @param k - the number of largest items
+	 * @param cmp - the comparator defining how to compare items
+	 * @return a list of the k largest items, in descending order
+	 * @throws IllegalArgumentException if k is negative or larger than the size of the given list
+	 */
+	public static <E> List<E> findKLargestHeap(List<E> items, int k, Comparator<? super E> cmp) throws IllegalArgumentException {
+		if (k < 0 || k > items.size())
+			throw new IllegalArgumentException();
+		List<E> result = new ArrayList<>();
+		BinaryMaxHeap<E> maxHeap = new BinaryMaxHeap<>(items, cmp);
+		for (int i = 0; i < k; i++)
+			result.add(maxHeap.extractMax());
+		return result;
+	}
+
+	/**
+	 * Determines the k largest items in the given list, using Java's sort routine and the 
+	 * natural ordering of the items.
+	 * 
+	 * @param items - the given list
+	 * @param k - the number of largest items
+	 * @return a list of the k largest items, in descending order
+	 * @throws IllegalArgumentException if k is negative or larger than the size of the given list
+	 */
+	public static <E extends Comparable<? super E>> List<E> findKLargestSort(List<E> items, int k) throws IllegalArgumentException {
+		if (k < 0 || k > items.size())
+			throw new IllegalArgumentException();
+		Collections.sort(items, Collections.reverseOrder());
+		List<E> kLargestItems = new ArrayList<>(k);
+
+		for (int i = 0; i < k; i++) {
+			kLargestItems.add(items.get(i));
+		}
+
+		return kLargestItems;
+	}
+
+	/**
+	 * Determines the k largest items in the given list, using Java's sort routine.
+	 * 
+	 * @param items - the given list
+	 * @param k - the number of largest items
+	 * @param cmp - the comparator defining how to compare items
+	 * @return a list of the k largest items, in descending order
+	 * @throws IllegalArgumentException if k is negative or larger than the size of the given list
+	 */
+	public static <E> List<E> findKLargestSort(List<E> items, int k, Comparator<? super E> cmp) throws IllegalArgumentException {
+		if (k < 0 || k > items.size())
+			throw new IllegalArgumentException();
+
+		Collections.sort(items, cmp.reversed());
+
+		// Create a new list to store the k largest items
+		List<E> kLargestItems = new ArrayList<>(k);
+
+		// Add the first k items to the new list
+		for (int i = 0; i < k; i++) {
+			kLargestItems.add(items.get(i));
+		}
+
+		return kLargestItems;
+	}
+}
